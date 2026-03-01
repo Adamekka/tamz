24097166bf62149fa3cb0b9043e2578405c6a6a4
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2026-03-01 14:28:18 +0100

    cv2 init

---
+++ cv1/capacitor.config.ts
@@ -0,0 +1,23 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const appId = 'cv1.ionic.io';
+const appName = 'cv1';
+const server = process.argv.includes('-hmr')
+	? {
+			url: 'http://10.10.0.213:5173', // always have http:// in url
+			cleartext: true
+		}
+	: {};
+const webDir = 'build';
+
+const config: CapacitorConfig = {
+	appId,
+	appName,
+	webDir,
+	server
+};
+
+if (process.argv.includes('-hmr'))
+	console.log('WARNING: running capacitor with livereload config', config);
+
+export default config;
